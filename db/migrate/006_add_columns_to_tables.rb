75beb27e6ae602b442b18337afe1b758030d0a85
jaechung77 <jaechung77@gmail.com>
2021-09-20 00:23:58 +1000

    lab

---
+++ db/migrate/006_add_columns_to_tables.rb
@@ -0,0 +1,8 @@
+class AddColumnsToTables < ActiveRecord::Migration[5.1]
+    def change
+        add_column :characters, :catchphrase, :string
+        add_column :shows, :day, :string
+        add_column :shows, :genre, :string
+        add_column :shows, :season, :string
+    end
+end        
